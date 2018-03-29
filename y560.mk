710b6d460d11ccbc97baa4d0a465912787d54b6d
Runner85sx <runner85sx@gmail.com>
2018-03-29 14:15:53 +0200

    [TEST] build QCamera2

--- y560.mk
+++ y560.mk
@@ -78,10 +78,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     gps.msm8909
 	
-# Camera
-PRODUCT_PACKAGES += \
-    camera.msm8909
-	
 # HIDL
 PRODUCT_COPY_FILES += \
     device/huawei/y560/manifest.xml:system/vendor/manifest.xml
@@ -97,9 +93,10 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl.legacy \
-    camera.device@3.2-impl \
-    camera.device@1.0-impl.legacy
+    android.hardware.camera.provider@2.4-impl-legacy \
+    camera.device@1.0-impl-legacy \
+    camera.msm8909 \
+    Camera2
 
 # Misc
 PRODUCT_PACKAGES += \
