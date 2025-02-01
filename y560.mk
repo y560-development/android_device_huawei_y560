faf685c25c038495dd30d9fe9f296501faf3b080
NotNoelChannel <notnoel12345678@gmail.com>
2025-02-01 12:55:48 +0100

    revert to snap

--- y560.mk
+++ y560.mk
@@ -89,10 +89,10 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
-    camera.device@1.0-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl \
     camera.msm8909 \
-    Camera2
+    Snap
 
 # Misc
 PRODUCT_PACKAGES += \
