3cc2cae0e5d7368eeefb75b249d3e47fd1c18059
NotNoelChannel <notnoel12345678@gmail.com>
2025-02-01 12:16:21 +0100

    Enforce vendor tree presence

--- y560.mk
+++ y560.mk
@@ -18,7 +18,7 @@ DEVICE_PACKAGE_OVERLAYS += device/huawei/y560/overlay
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
-$(call inherit-product-if-exists, vendor/huawei/y560/y560-vendor.mk)
+$(call inherit-product, vendor/huawei/y560/y560-vendor.mk)
 
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := hdpi
